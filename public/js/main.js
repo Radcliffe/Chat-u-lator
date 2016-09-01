85a828a6e1798d693144113a18dc90fc5f2aae67
David Radcliffe <dradcliffe@gmail.com>
2016-09-01 10:00:00 -0500

    add informative error messages and jslint compliance

--- public/js/main.js
+++ public/js/main.js
@@ -1,48 +1,54 @@
-$(document).ready(function() {
+/*global $, io*/
+
+$(document).ready(function () {
+    'use strict';
 
     var numEquations = 0,
         expr = $('#expr'),
         resultList = $('#result-list'),
         errorMessage = $('#error-message'),
+        errorText = $('#error-text'),
         dismissError = $('#dismiss'),
         inputForm = $('form'),
         socket = io();
 
     errorMessage.hide();
 
-    $.get('/api/v1/calc', function(data) {
+    function makeItem(equation) {
+        return '<li class="list-group-item">' + equation + '</li>';
+    }
+
+    $.get('/api/v1/calc', function (data) {
         numEquations = data.length;
         data.forEach(function (item) {
             resultList.append(makeItem(item.text));
         });
     });
 
-    inputForm.submit(function() {
+    inputForm.submit(function () {
         errorMessage.hide();
         socket.emit('message', expr.val());
         expr.val('');
         return false;
     });
 
-    dismissError.click(function() {
+    dismissError.click(function () {
         errorMessage.hide();
     });
 
-    socket.on('message', function(equation) {
+    socket.on('message', function (equation) {
         resultList.prepend(makeItem(equation));
 
         if (numEquations === 10) {
             $('li').last().remove();
         } else {
-            numEquations++;
+            numEquations += 1;
         }
     });
 
-    socket.on('err', function(msg) {
+    socket.on('err', function (msg) {
+        errorText.text(msg);
         errorMessage.show();
     });
 
-    function makeItem(equation) {
-        return '<li class="list-group-item">' + equation + '</li>';
-    }
 });
--- server.js
+++ server.js
@@ -1,86 +1,85 @@
-var express = require('express');
-var app = express();
-var http = require('http');
-var server = http.Server(app);
-var io = require('socket.io')(server);
-var sanitizeHtml = require('sanitize-html');
-var bodyParser = require('body-parser')
-var math = require('mathjs');
-
-var pg = require('pg');
-var connectionString = process.env.ConnectionString;
-
-app.use(express.static('public'));
-
-var PORT = process.env.PORT || 3000;
-
-app.use(bodyParser.urlencoded({ extended: false }))
-
-app.get('/', function(request, response) {
-    response.sendFile('index.html');
-});
-
-io.on('connection', function(socket) {
-    socket.on('message', function (m) {
-        var clean = sanitizeHtml(m, {
-            allowedTags: [],
-            allowedAttributes: []
-        });
-
-        if (m != clean) {
-            return socket.emit('err', 'HTML not allowed');
-        }
-
-        try {
-            var value = math.eval(m);
-            var equation = m + " = " + value;
-            io.emit('message', equation);
-            save(equation);
-        }
-        catch (err) {
-            return socket.emit('err', 'Unable to evaluate expression');
-        }
-
-    });
-});
+let express = require('express'),
+    app = express(),
+    http = require('http'),
+    server = http.Server(app),
+    io = require('socket.io')(server),
+    sanitizeHtml = require('sanitize-html'),
+    bodyParser = require('body-parser'),
+    math = require('mathjs'),
+    pg = require('pg'),
+    connectionString = process.env.ConnectionString,
+    PORT = process.env.PORT || 3000;
 
 function save(equation) {
-    pg.connect(connectionString, function(err, client, done) {
+    pg.connect(connectionString, function (err, client, done) {
         if (err) {
             done();
             console.log(err);
         }
 
-        var query = client.query('INSERT INTO calc(text) values($1)', [equation]);
+        let query = client.query('INSERT INTO calc(text) values($1)', [equation]);
 
-        query.on('end', function() {
+        query.on('end', function () {
             done();
         });
     });
 }
 
+app.use(express.static('public'));
 
-app.get('/api/v1/calc', function(req, res) {
-    var results = [];
-    pg.connect(connectionString, function(err, client, done) {
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', function (request, response) {
+    response.sendFile('index.html');
+});
+
+io.on('connection', function (socket) {
+    socket.on('message', function (m) {
+        let clean = sanitizeHtml(m, {
+            allowedTags: [],
+            allowedAttributes: []
+        });
+
+        if (m !== clean) {
+            return socket.emit('err', 'HTML not allowed');
+        }
+
+        try {
+            let value = math.eval(m),
+                equation = m + " = " + value;
+            io.emit('message', equation);
+            save(equation);
+        } catch (err) {
+            return socket.emit('err', err.message);
+        }
+
+    });
+});
+
+
+
+
+app.get('/api/v1/calc', function (req, res) {
+    let results = [];
+    pg.connect(connectionString, function (err, client, done) {
         if (err) {
             done();
             console.log(err);
             return res.status(500).json({success: false, data: err});
         }
-        var query = client.query('SELECT * FROM calc ORDER BY id DESC LIMIT 10;');
+        let query = client.query('SELECT * FROM calc ORDER BY id DESC LIMIT 10;');
 
-        query.on('row', function(row) {
+        query.on('row', function (row) {
             results.push(row);
         });
 
-        query.on('end', function() {
+        query.on('end', function () {
             done();
             return res.json(results);
         });
     });
 });
 
-server.listen(PORT, function() {
+server.listen(PORT, function () {
     console.log('Listening on Port', PORT);
 });
